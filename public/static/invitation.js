bdaf11697d647334715392f43f7a3518195d00c2
黄文希 <wen2237380164@126.com>
2020-07-31 18:32:44 +0800

    11112212

--- public/static/invitation.js
+++ public/static/invitation.js
@@ -1,6 +1,6 @@
 let nodes = new Map(); //所有控件的集合
 let idMap = new Map(); //所有控件的集合
-let nodeStyleMap = new Map(); //所有控件的集合
+let nodeStyleMap = new Map(); // 所有控件的集合
 
 
 let mouseIsDown = false;
@@ -9,20 +9,70 @@ let mouseX = 0;
 let mouseY = 0;
 let nodeX = 0;
 let nodeY = 0; 
-let nodeWidth;
+let nodeWidth; 
 let nodeHeight;
 
 let moveMethod;
+let vue = null
+let defaultStyle = null
+// 文本
+var textStyle = {
+	"opacity": 100, // 透明度
+	// "backColor": "#FFFFFF", // 文本背景颜色
+	"borderColor": "#2c3e50", // 文本边框颜色
+	"borderStyle":"none", // 边框状态  直线 虚线 双直线
+	"borderWidth":0, // 边框宽度
+	"borderRadius":0, // 边框弧形角度
+	"shadowWidth":0,  // 阴影大小
+	"shadowColor":"#000",  // 阴影颜色
+	"shadowDim":0,   // 阴影模糊程度
+	"shadowDirectionV":0,  // 阴影水平方向移动
+	"shadowDirectionH":0,  // 阴影垂直方向移动
+	"z-index": '5', // 元素层级 最底层：1 默认：5 最顶层：9
+	"width": '60%', // 文本基础宽度
+	"height": '40px', // 文本基础高度
+	'el-x': '', // 创建的文本元素距离模板左的距离
+	'el-y': '', // 创建的文本元素距离模板上的距离
 
 
+	"textColor": "#2c3e50", // 字体颜色
+	"fontFamily": "微软雅黑", // 字体
+	"fontSize": "14px", // 字体大小
+	"lineSpa": 0, // 字体间距
+	"lineHeight": 16, // 行高
+	"textAlign": "left", // 文本对齐方式 left center right
+	"fontWeight": false, // 字体粗细
+	"textDecoration": false, // 文本下划线
+	"fontStyle": false, // 文本斜体
+}
+
+// 图片
+var imgStyle = {
+	"opacity": 100, // 透明度
+	// "backColor": "#FFFFFF", // 背景颜色
+	"borderColor": "#2c3e50", // 边框颜色
+	"borderStyle":"none", // 边框状态  直线 虚线 双直线
+	"borderWidth":0, // 边框宽度
+	"borderRadius":0, // 边框弧形角度
+	"shadowWidth":0,  // 阴影大小
+	"shadowColor":"#000",  // 阴影颜色
+	"shadowDim":0,   // 阴影模糊程度
+	"shadowDirectionV":0,  // 阴影水平方向移动
+	"shadowDirectionH":0,  // 阴影垂直方向移动
+	"z-index": '5', // 元素层级 最底层：1 默认：5 最顶层：9
+	'el-x': '', // 创建的文本元素距离模板左的距离
+	'el-y': '', // 创建的文本元素距离模板上的距离
+	"width": '120px', // 文本基础宽度
+	"height": '120px', // 文本基础高度
+	
+	"url": '', // 图片链接
+}
 
 import $ from 'jquery'
-
 
 window.allowDrop = function(event) {
 	event.preventDefault();
 }
-
 
 function textDrag(event) { 
 	event = event || window.event
@@ -36,11 +86,17 @@ export function textDragover(event) {
 }
 
 export function drop(event, _this) {
+	// 去除事件冒泡
 	event = event || window.event
 	console.log(event)
 	event.stopPropagation();
 	event.preventDefault();
-	// let data='image'
+	
+	// 保存 vue 对象
+	vue = _this
+
+	window.v = _this
+	// 获取元素类型
 	let data = event.dataTransfer.getData("text");
 	//如果不是拖动的文本框，不响应
 	console.log(data)
@@ -50,65 +106,102 @@ export function drop(event, _this) {
 	let node 
 	var nodeValue;
 	if (data == 'invite-text') {
-		nodeValue = "文本333";
+		nodeValue = "文本";
 		node = document.getElementById('textTemplate').cloneNode(true);
+		// 将元素 默认数据 统一保存在defaultStyle 对象中
+		defaultStyle = textStyle
+		
+		// 元素样式配置
+		_this.isImage = false
 	} else if (data == 'image') {
-		nodeValue = "图片111";
+		nodeValue = "图片";
 		node = document.getElementById('imageTemplate').cloneNode(true);
+		// 将元素 默认数据 统一保存在defaultStyle 对象中
+		defaultStyle = imgStyle
+		// 元素样式配置
+		_this.isImage = true
 	}
+
+	// 保存类型
+	defaultStyle.type = data
 
 	if (!node) {
 		return;
 	}
+	
 	hideBox()
 	console.log(event)
 	//确定鼠标位置
 	let x = event.layerX - 80;
 	let y = event.layerY - 20;
 	console.log("xy = " + x, y)
-	//复制一个文件编辑控件
+	// 将当前dom 放入vue.tNode 中
+	_this.tNode = node
+	_this.defaultStyle = defaultStyle
 
-	
 	console.log(node)
 	// 配置唯一ID
 	node.id = uuid()
 	console.log("node = "+node)
-	var idObject = {};
-	idObject.id = node.id;
-	idObject.nodeValue=nodeValue;
-	_this.idList.push(idObject)
+
+	/**
+	 * 数据收集  dataCollection
+	 * 判断是否存在当前页的 数据 
+	 * 		判断机制：当前页码(curPage) > 数据长度(dataCollection.length)	?
+	 * 						 不存在 : 存在
+	 * 
+	 *    不存在：创建一条新记录,
+	 *      { 
+						page: '1',  // 页码标识
+						dataPre: [],  // 每页对应的元素映射
+						eleList: [] // 保存每页的元素列
+					}
+					在dataPre中添加新 映射对象
+					在eleList中添加新 记录
+
+				存在： 找到与当前页码对应的记录 
+					在dataPre中添加新 映射对象 
+					在eleList中添加新 记录
+	 */
+
+	if(_this.curPage > _this.dataCollection.length){
+		// 创建新记录
+		_this.dataCollection.push({
+			page: _this.curPage,
+			dataPre: [],
+			eleList: []
+		})
+	} 
+
+	// 插入新的 映射对象
+	_this.dataCollection[_this.curPage - 1].dataPre.push({key: node.id, value: defaultStyle})
+	// 插入新的 元素列
+	_this.dataCollection[_this.curPage - 1].eleList.push({ id: node.id, nodeValue })
+
+
+	// 元素列
+	_this.idList.push({ id: node.id, nodeValue })
+
 	idMap.set(node.id,_this.showKey)
-	var defaultStyle = {"textColor": "#2c3e50",
-					"fontFamily": "微软雅黑",
-					"fontSize": "14px",
-					"lineSpa": 0,
-					"lineHeight": 16,
-					"opacity": 100,
-					"textAlign": "",
-					"fontWeight": false,
-					"textDecoration": false,
-					"fontStyle": false,
-					"backColor": "#FFFFFF",
-					"borderColor": "#2c3e50",
-					"borderStyle":"none",
-					"borderWidth":0,
-					"borderRadius":0,
-					"shadowWidth":0,  //大小
-					"shadowColor":"#000",  //颜色
-					"shadowDim":0,   //模糊
-					"shadowDirectionV":0,  // 水平
-					"shadowDirectionH":0,  //垂直方向
-}
+	
 	nodeStyleMap.set(node.id,defaultStyle)
 	
-	
-	// 设置控件位置和样式
-	$(node).css('display', 'block')
-	$(node).css('position', 'absolute')
-	console.log("设置 xy = " + x, y)
+	// 设置元素位置和样式
+	// $(node).css('display', 'block')
+	// $(node).css('position', 'absolute')
+	// console.log("设置 xy = " + x, y)
 	$(node).css('transform', "translate(" + x + "px," + y + "px)")
+
+	// 将坐标x, y 保存默认数据中 defaultStyle
+	defaultStyle['el-x'] = x
+	defaultStyle['el-y'] = y
+
 	// 把控件保存起来
 	nodes.set(node.id, node)
+
+	// 渲染 当前dom
+	renderingDom(data, defaultStyle)
+
 	// 给控件绑定点击事件
 	$(node).click(function(e) {
 		e.stopPropagation()
@@ -126,7 +219,7 @@ export function drop(event, _this) {
 			 _this.activeName.push("2")
 			 _this.activeName.push("3")
 			}
-		_this.initTemplateCss(this)
+		// _this.initTemplateCss(this)
 		// $('#baseStyle').click();
 	})
 
@@ -142,10 +235,10 @@ export function drop(event, _this) {
 
 	$(node).mousedown(function(e) {
 		// 鼠标按下时，初始化当前控件的各项属性
-		if ($(e.target).hasClass('edit-text')) {
-			// moveMethod='topResize'
-			return
-		}
+		// if ($(e.target).hasClass('edit-text')) {
+		// 	// moveMethod='topResize'
+		// 	return
+		// }
 		mouseIsDown = true;
 		currentNode = this;
 		mouseX = e.pageX;
@@ -177,14 +270,58 @@ export function drop(event, _this) {
 			moveMethod = 'rightBottomResize'
 		}
 
-
-
 	});
 	//把控件添加到容器内
 	event.target.appendChild(node);
 	// 触发一次控件的点击事件
 	$(node).click()
 
+}
+
+// 渲染当前 元素dom
+function renderingDom(elType, obj){
+	// 当前dom
+	let tNode = vue.tNode
+	console.log(tNode, obj)
+
+	// 给tNode 编写css属性
+	tNode.style = `
+		opacity: ${obj.opacity / 100};
+		border-color: ${obj.borderColor};
+		border-style: ${obj.borderStyle};
+		border-width: ${obj.borderWidth}px';
+		border-radius: ${obj.borderRadius}px;
+		z-index: ${obj['z-index']};
+		box-shadow: ${obj.shadowWidth}px ${obj.shadowDirectionV}px ${obj.shadowDirectionH}px ${obj.shadowDim}px ${obj.shadowColor};
+		width: ${obj.width};
+		height: ${obj.height};
+		transform: translate(${obj['el-x']}px, ${obj['el-y']}px);
+	`
+
+	// 文本
+	if(elType == 'invite-text'){
+		var alonePos = [
+			{css: 'color', self: 'textColor', company: ''},
+			{css: 'font-family', self: 'fontFamily', company: ''},
+			{css: 'font-size', self: 'fontSize', company: 'px'},
+			{css: 'letter-spacing', self: 'lineSpa', company: 'px'},
+			{css: 'line-height', self: 'lineHeight', company: 'px'},
+			{css: 'text-align', self: 'textAlign', company: ''},
+			{css: 'text-weight', self: 'fontWeight', company: ''},
+			{css: 'text-decoration', self: 'textDecoration', company: ''},
+			{css: 'font-style', self: 'fontStyle', company: ''}
+		]
+
+		for(let i in alonePos){
+			tNode.style[alonePos[i].css] = obj[alonePos[i].self] + alonePos[i].company
+		}
+
+	} else 
+	// 图片
+	if(elType == 'image'){
+		// url
+		tNode.style.backgroundColor = obj['url']
+	}
 }
 
 export function addSubmitForm(_this) {
@@ -293,13 +430,115 @@ export function initNode(node, _this,text) {
 	});
 }
 
-
-
+// 生成唯一id
 function uuid() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 		var r = Math.random() * 16 | 0,
 			v = c == 'x' ? r : (r & 0x3 | 0x8);
 		return v.toString(16);
+	});
+}
+
+// 还原元素
+export function reduction(_this){
+
+	console.log('reduction')
+	// 创建dom
+	let dataCollection = _this.dataCollection
+	let curPage = _this.curPage
+	let node
+	vue = _this
+
+	for(let item of dataCollection[curPage - 1].dataPre){
+		var data = item.value.type
+		
+		if (data == 'invite-text') {
+			node = document.getElementById('textTemplate').cloneNode(true);
+			// 将元素 默认数据 统一保存在defaultStyle 对象中
+			defaultStyle = item.value
+			
+			// 元素样式配置
+			_this.isImage = false
+		} else if (data == 'image') {
+			node = document.getElementById('imageTemplate').cloneNode(true);
+			// 将元素 默认数据 统一保存在defaultStyle 对象中
+			defaultStyle = item.value
+			// 元素样式配置
+			_this.isImage = true
+		}
+
+		node.id = item.key
+		
+		_this.tNode = node
+		// 渲染dom
+		renderingDom(data, defaultStyle)
+		// 元素附加拖动效果
+		dropEffect(node, _this)
+
+		document.querySelector('.phone-item').appendChild(node)
+	}
+}
+
+// 元素附加拖动效果
+function dropEffect(node, _this){
+	$(node).click(function(e) {
+		e.stopPropagation()
+		e.preventDefault()
+		hideBox()
+		$(this).find('.invite-text-box-border').css('display', 'block')
+		// _this.initTemplateCss(this)
+		
+		if(_this.activeName.length === 0){
+			_this.activeName.push("1")
+			 _this.activeName.push("2")
+			 _this.activeName.push("3")
+		}
+			
+	})
+	$(node).mousedown(function(e) {
+
+		$(".check").removeClass("check")
+		 $("#" +'itemId'+ node.id).addClass("check");
+		
+		// var nodeElement = $("#"+ node.id);
+		// console.log("zzzz",nodeElement.css('color'))
+		// 鼠标按下时，初始化当前控件的各项属性
+		// if ($(e.target).hasClass('edit-text')) {
+		// 	// moveMethod='topResize'
+		// 	return
+		// }
+		console.log("mouseIsDown = " + mouseIsDown)
+		mouseIsDown = true;
+		currentNode = this;
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+		moveMethod = 'move';
+		let trans = $(this).css('transform')
+		let s = trans.split(',');
+		nodeWidth = $(this).css('width').replace('px', '') - 0;
+		nodeHeight = $(this).css('height').replace('px', '') - 0
+		console.log(nodeWidth, nodeHeight, s)
+		nodeX = s[4] - 0
+		nodeY = s[5].substr(0, s[5].length - 1) - 0
+		console.log($(e.target).hasClass('top-line-point'))
+		if ($(e.target).hasClass('top-line-point')) {
+			moveMethod = 'topResize'
+		} else if ($(e.target).hasClass('left-line-point')) {
+			moveMethod = 'leftResize'
+		} else if ($(e.target).hasClass('right-line-point')) {
+			moveMethod = 'rightResize'
+		} else if ($(e.target).hasClass('bottom-line-point')) {
+			moveMethod = 'bottomResize'
+		} else if ($(e.target).hasClass('left-top-point')) {
+			moveMethod = 'leftTopResize'
+		} else if ($(e.target).hasClass('right-top-point')) {
+			moveMethod = 'rightTopResize'
+		} else if ($(e.target).hasClass('left-bottom-point')) {
+			moveMethod = 'leftBottomResize'
+		} else if ($(e.target).hasClass('right-bottom-point')) {
+			moveMethod = 'rightBottomResize'
+		}
+
 	});
 }
 
@@ -351,8 +590,6 @@ document.body.onmousemove = function(event) {
 	} else if (moveMethod == 'rightResize') {
 		rightResize(moveX, moveY)
 	}
-
-
 }
 
 function move(moveX, moveY) {
